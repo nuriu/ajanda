c7590bca2c0b69bc0d453390cb7c7b1a85240ddd
Nuri Uzunoğlu <nuriu@protonmail.com>
2018-01-13 11:14:10 +0300

    added moment dependency.

--- src/app/providers/data.service.ts
+++ src/app/providers/data.service.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import * as fs from 'fs';
 import * as SQL from 'sql.js';
+
 @Injectable()
 export class DataService {
   private fileBuffer: Buffer;
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -65,5 +65,3 @@ import 'zone.js/dist/zone-mix'; // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
-
-import 'sql.js'
