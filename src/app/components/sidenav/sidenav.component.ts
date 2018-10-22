f6469d1aa6ae5b1d7136e48a7cb859ae905ff93f
Nuri Uzunoğlu <nuriu@protonmail.com>
2018-10-22 14:53:50 +0400

    added basic tag selection at logic level.

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Calendar } from '../../models/Calendar';
+import { Tag } from '../../models/Tag';
 import { DataService } from '../../services/data.service';
 import { EVENT_TYPES, LoggerService, LOG_LEVELS } from '../../services/logger.service';
 
@@ -17,6 +18,10 @@ export class SidenavComponent implements OnInit {
    * List of calendars.
    */
   calendars: Array<Calendar>;
+  /**
+   * List of tags.
+   */
+  tags: Array<Tag>;
 
   constructor(private db: DataService, private logger: LoggerService) {
     this.calendars = new Array<Calendar>();
@@ -27,6 +32,7 @@ export class SidenavComponent implements OnInit {
 
     this.databaseName = this.db.getDatabaseName();
     this.calendars = this.db.listCalendars();
+    this.tags = this.db.listTags();
   }
 
   /**
@@ -52,6 +58,32 @@ export class SidenavComponent implements OnInit {
   handleCalendarAddition(name: string) {
     if (name.trim().length > 0) {
       this.db.createCalendar(new Calendar({ name: name }));
+    }
+  }
+
+  /**
+   * Updates tag selection info.
+   * @param tag Selected tag.
+   */
+  handleTagSelection(tag: Tag) {
+    tag.enabled = !tag.enabled;
+
+    this.db.updateTag(tag);
+
+    this.logger.log(
+      'Tag selection updated: ' + JSON.stringify(tag),
+      LOG_LEVELS.EVENT,
+      EVENT_TYPES.CLICK
+    );
+  }
+
+  /**
+   * Handles new tag addition.
+   * @param name Name for tag to add.
+   */
+  handleTagAddition(name: string) {
+    if (name.trim().length > 0) {
+      this.db.createTag(new Tag({ name: name }));
     }
   }
 }
--- src/app/models/Tag.ts
+++ src/app/models/Tag.ts
@@ -1,11 +1,13 @@
 export class Tag {
   public id: string;
   public name: string;
+  public colorCode: string;
   public enabled: boolean;
 
   public constructor(init?: Partial<Tag>) {
     this.id = '';
     this.name = '';
+    this.colorCode = '';
     this.enabled = false;
 
     if (init) {
